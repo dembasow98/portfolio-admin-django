cfedc5242f10ef4b6d710e926455caa3d230c77c
dembasow98 <dastech1998@gmail.com>
2023-06-20 10:51:18 +0300

    Edited settings.py file.

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ge!y*bo(aa+1@chy)*5f7#km)b694!u6)9$3n4+q=@(mo-^xn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [] # Allow *.vercel.app
 
